0947daaf3b2a4cf9c19d67a65bc9d5b4374b2301
bBraian <braianvoficial@gmail.com>
2025-04-27 18:21:21 -0300

    feat: validating the requests

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -1,8 +1,19 @@
 <h1>Nova duvida</h1>
 
+@if ($errors->any())
+    <div>
+        <h2>Erros</h2>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
 <form action="{{ route('supports.store') }}" method="POST">
     @csrf()
-    <input type="text" placeholder="Assunto" name="subject">
-    <textarea name="body" cols="30" rows="5" placeholder="Descricao"></textarea>
+    <input type="text" placeholder="Assunto" name="subject" value="{{ old('subject') }}">
+    <textarea name="body" cols="30" rows="5" placeholder="Descricao">{{ old('body') }}</textarea>
     <button type="submit">Enviar</button>
 </form>
--- resources/views/admin/supports/edit.blade.php
+++ resources/views/admin/supports/edit.blade.php
@@ -1,5 +1,16 @@
 <h1>Duvida {{ $support->id }}</h1>
 
+@if ($errors->any())
+    <div>
+        <h2>Erros</h2>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
 <form action="{{ route('supports.update', $support->id) }}" method="POST">
     @csrf()
     @method('PUT')
