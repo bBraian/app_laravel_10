2e466c6d890bfdfce5e8522912875f154add2c07
bBraian <braianvoficial@gmail.com>
2025-05-04 15:39:23 -0300

    feat: implement service layer

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -5,27 +5,32 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUpdateSupport;
 use App\Models\Support;
+use App\Services\SupportService;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index(Support $support)
+    public function __construct(protected SupportService $service)
     {
-        $supports = $support->all();
+    }
+
+    public function index(Request $request)
+    {
+        $supports = $this->service->getAll($request->filter);
         return view('admin/supports/index', compact('supports'));
     }
 
-    public function show(string|int $id, Support $support)
+    public function show(string $id, Support $support)
     {
-        if(!$support = $support->find($id)) {
+        if(!$support = $this->service->findOne($id)) {
             return back();
         }
         return view('admin/supports/show', compact('support'));
     }
 
-    public function edit(string|int $id, Support $support)
+    public function edit(string $id)
     {
-        if(!$support = $support->find($id)) {
+        if(!$support = $this->service->findOne($id)) {
             return back();
         }
         return view('admin/supports/edit', compact('support'));
@@ -42,13 +47,9 @@ class SupportController extends Controller
         return redirect()->route('supports.index');
     }
 
-    public function destroy(string|int $id, Support $support)
+    public function destroy(string $id)
     {
-        if(!$support = $support->find($id)) {
-            return back();
-        }
-
-        $support->delete();
+        $this->service->delete($id);
         return redirect()->route('supports.index');
     }
 
